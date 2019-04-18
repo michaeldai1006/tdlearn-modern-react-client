a6569a32d2b3581bf623165aff4b1fd48c260cf3
michaeldai1006 <dtc74110@gmail.com>
2019-04-17 22:50:23 -0700

    Validation of Form Inputs

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -24,6 +24,20 @@ class StreamCreate extends React.Component {
             </form>
         );
     }
+}
+
+const validate = (formValues) => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
 };
 
 export default reduxForm({
