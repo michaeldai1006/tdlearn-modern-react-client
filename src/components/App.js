b67c4e81066c80fb2103fb558dde434d844482e2
Michael Dai <michaeldai@Michaels-MacBook-Pro.local>
2019-05-11 12:37:02 -0700

    Linking to Stream Creation

--- src/components/App.js
+++ src/components/App.js
@@ -14,10 +14,10 @@ const App = () => {
                 <div>
                     <Header />
                     <Route path="/" exact component={StreamList} />
-                    <Route path="/stream/new" component={StreamCreate} />
-                    <Route path="/stream/edit" component={StreamEdit} />
-                    <Route path="/stream/delete" component={StreamDelete} />
-                    <Route path="/stream/show" component={StreamShow} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
                 </div>
             </BrowserRouter>
         </div>
